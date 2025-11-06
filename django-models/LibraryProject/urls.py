c83a34e0fc828ee8bb715017b4d01f84a3dd09a7
pshluvu <93921716+pshluvu@users.noreply.github.com>
2025-11-06 23:46:44 +0200

    Remove duplicate include statements in urls.py

--- django-models/LibraryProject/urls.py
+++ django-models/LibraryProject/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path, include
+from django.urls import path, include
+
 
 urlpatterns = [
     path('', include('relationship_app.urls')),
+    path('', include('relationship_app.urls')),
+
 ]
