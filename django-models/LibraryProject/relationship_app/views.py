10092c9aee0a42d53d35838098d1b5c270c84d13
pshluvu <93921716+pshluvu@users.noreply.github.com>
2025-11-06 22:44:59 +0200

    Update views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.generic import DetailView
+from .models import Book, Library
+
 
 # Create your views here.
