b15a48bc6986b5f365a76d7dcd478a64e907bd18
pshluvu <93921716+pshluvu@users.noreply.github.com>
2025-11-07 00:05:46 +0200

    Add role-based views to URL patterns

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -5,6 +5,10 @@ from .views import register_view, CustomLoginView, CustomLogoutView
 from django.urls import path
 from . import views
 from django.contrib.auth.views import LoginView, LogoutView
+from django.urls import path
+from .views import admin_view, librarian_view, member_view
+
+
 
 
 urlpatterns = [
@@ -16,5 +20,9 @@ urlpatterns = [
     path('register/', views.register_view, name='register'),
     path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
+    path('admin-role/', admin_view, name='admin_view'),
+    path('librarian-role/', librarian_view, name='librarian_view'),
+    path('member-role/', member_view, name='member_view'),
+
 
 ]
