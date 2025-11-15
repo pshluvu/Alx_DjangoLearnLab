4fcca1f38b1e28c15a1ff854a8a81845185ac444
pshluvu <93921716+pshluvu@users.noreply.github.com>
2025-11-15 23:02:57 +0200

    Delete advanced_features_and_security directory

--- advanced_features_and_security/accounts/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-def home(request):
-    return HttpResponse("Welcome to the Advanced Features and Security Project!")
-
-# Create your views here.
