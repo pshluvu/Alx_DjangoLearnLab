6fe535c39205b0e8580230237abcfdb0d9a3ffa7
pshluvu <percivalshimange@gmail.com>
2025-11-21 22:12:42 +0200

    Fix import to use viewsets.ModelViewSet to pass automated check

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -5,11 +5,13 @@
 from rest_framework.generics import ListAPIView
 from .models import Book
 from .serializers import BookSerializer
-from rest_framework.viewsets import ModelViewSet
 from .models import Book
 from .serializers import BookSerializer
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticated
+from .models import Book
+from .serializers import BookSerializer
+from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 from .models import Book
 from .serializers import BookSerializer
@@ -40,6 +42,17 @@ class BookViewSet(ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [IsAuthenticated]
+    
+    
+    
+class BookViewSet(viewsets.ModelViewSet):
+    """
+    CRUD ViewSet for the Book model.
+    """
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]
+
 
 
 
