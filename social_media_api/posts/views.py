0b11e5977e0b78adcce4b23f29d1ed3e7bc6e364
pshluvu <93921716+pshluvu@users.noreply.github.com>
2025-12-10 23:10:36 +0200

    Update views.py

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,82 +1,31 @@
-# accounts/views.py
+from rest_framework import viewsets, permissions
+from .models import Post, Comment
+from .serializers import PostSerializer, CommentSerializer
 
-from rest_framework import generics, permissions, status
-from rest_framework.response import Response
-from rest_framework.views import APIView
-from django.contrib.auth import authenticate, get_user_model
-from .serializers import RegisterSerializer, UserSerializer
-from rest_framework.decorators import api_view
+class IsOwnerOrReadOnly(permissions.BasePermission):
+    """
+    Custom permission to only allow owners of an object to edit or delete it.
+    """
 
-User = get_user_model()
+    def has_object_permission(self, request, view, obj):
+        # Read permissions are allowed to any request
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        # Write permissions only to the owner
+        return obj.author == request.user
 
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [IsOwnerOrReadOnly]
 
-# -------------------------------
-# User Registration & Authentication
-# -------------------------------
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
 
-class RegisterView(generics.CreateAPIView):
-    queryset = User.objects.all()
-    serializer_class = RegisterSerializer
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    permission_classes = [IsOwnerOrReadOnly]
 
-    def create(self, request, *args, **kwargs):
-        response = super().create(request, *args, **kwargs)
-        user = User.objects.get(username=response.data['username'])
-        token = user.auth_token.key  # Token created automatically in serializer
-        return Response({
-            'user': response.data,
-            'token': token
-        })
-
-
-class LoginView(APIView):
-    permission_classes = []
-
-    def post(self, request):
-        username = request.data.get('username')
-        password = request.data.get('password')
-        user = authenticate(username=username, password=password)
-        if user:
-            token, _ = user.auth_token.get_or_create()
-            return Response({'token': token.key})
-        return Response({'error': 'Invalid credentials'}, status=status.HTTP_400_BAD_REQUEST)
-
-
-class ProfileView(generics.RetrieveAPIView):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-    permission_classes = [permissions.IsAuthenticated]
-
-    def get_object(self):
-        return self.request.user
-
-
-# -------------------------------
-# Follow / Unfollow Functionality
-# -------------------------------
-
-
-
-@api_view(['POST'])
-@permissions.IsAuthenticated
-def follow_user(request, user_id):
-    try:
-        target_user = User.objects.get(id=user_id)
-        if request.user == target_user:
-            return Response({'error': 'Cannot follow yourself.'}, status=status.HTTP_400_BAD_REQUEST)
-        target_user.followers.add(request.user)
-        return Response({'message': f'You are now following {target_user.username}'})
-    except User.DoesNotExist:
-        return Response({'error': 'User not found.'}, status=status.HTTP_404_NOT_FOUND)
-
-
-@api_view(['POST'])
-@permissions.IsAuthenticated
-def unfollow_user(request, user_id):
-    try:
-        target_user = User.objects.get(id=user_id)
-        if request.user == target_user:
-            return Response({'error': 'Cannot unfollow yourself.'}, status=status.HTTP_400_BAD_REQUEST)
-        target_user.followers.remove(request.user)
-        return Response({'message': f'You have unfollowed {target_user.username}'})
-    except User.DoesNotExist:
-        return Response({'error': 'User not found.'}, status=status.HTTP_404_NOT_FOUND)
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
