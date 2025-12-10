69d729da31f29e9e120ef1a514ceb437e67a3296
pshluvu <93921716+pshluvu@users.noreply.github.com>
2025-12-10 23:15:07 +0200

    Update views.py

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -1,8 +1,9 @@
-from rest_framework import generics, permissions, status
+from rest_framework import generics, permissions, status, viewsets
 from rest_framework.response import Response
-from .models import CustomUser
-from .serializers import UserSerializer
+from .models import CustomUser, Post, Comment
+from .serializers import UserSerializer, PostSerializer, CommentSerializer
 
+# Follow/Unfollow views
 class FollowUserView(generics.GenericAPIView):
     queryset = CustomUser.objects.all()
     serializer_class = UserSerializer
@@ -20,6 +21,7 @@ class FollowUserView(generics.GenericAPIView):
         request.user.following.add(user_to_follow)
         return Response({"detail": f"You are now following {username}."}, status=status.HTTP_200_OK)
 
+
 class UnfollowUserView(generics.GenericAPIView):
     queryset = CustomUser.objects.all()
     serializer_class = UserSerializer
@@ -36,4 +38,36 @@ class UnfollowUserView(generics.GenericAPIView):
         
         request.user.following.remove(user_to_unfollow)
         return Response({"detail": f"You have unfollowed {username}."}, status=status.HTTP_200_OK)
+
+
+# Feed view for posts from followed users
+class FeedView(generics.ListAPIView):
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        following_users = user.following.all()
+        return Post.objects.filter(author__in=following_users).order_by('-created_at')
+
+
+# CRUD viewsets for posts
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+
+# CRUD viewsets for comments
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
 
